d1526a3d082cd73279c8106e30a33615aebbaf5c
Harrison Kaplan <harrisonkaplan@dhcp-18-189-17-195.dyn.MIT.EDU>
2018-01-27 16:25:30 -0500

    implemented pagination

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
-
 access_key = "077ab2a1817aecb1dbacb4cc58439f447eeffc76"
 
 device1 = "210042001747343338333633"
 device2 = "340037001147353136383631"
 # nested_dictionaries = {projectnumber:{step#:{device_id:x,command:y}}}
 projectData = {1:
-                [{"device_id":device1,"function":"step1"},
+                [{"name":"Project 1"},
+                 {"device_id":device1,"function":"step1"},
                  {"device_id":device1,"function":"step2"},
 
                  {"device_id":device2,"function":"step3"},
                  {"device_id":device2,"function":"step4"}],
                2:
-                [{"device_id":device1,"function":"step1"},
+                [{"name":"Project 2"},
+                {"device_id":device1,"function":"step1"},
                 {"device_id":device2,"function":"step3"},
                  {"device_id":device1,"function":"step2"},
 
@@ -25,13 +23,25 @@ projectData = {1:
                }
 
 
-@app.route('/project1')
-def project1():
-    return render_template('project1.html',access_key=access_key,project_data=projectData[1])
 
-@app.route('/project2')
-def project2():
-    return render_template('project1.html',access_key=access_key,project_data=projectData[2])
+@app.route('/')
+def home():
+    projects = []
+    for key, value in sorted(projectData.items()):
+        print(key,value)
+        projects.append((key,value[0]['name']))
+
+    return render_template('index.html',projects = projects)
+
+
+@app.route('/projects/', defaults={'project': 1})
+@app.route('/projects/<int:project>')
+def show_users(project):
+    return render_template('project1.html',
+        access_key=access_key,
+        project_data = projectData[project]
+    )
+
 
 if __name__ == "__main__":
     app.run(port=5000,debug = True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,31 +1,39 @@
-// $(document).ready( function(){
-//   var step = document.getElementById('step').value;
-//   console.log(step)
-//   $('#forms').submit(function(step){
-//     $.ajax({
-//           type: 'POST',
-//           async: false,
-//           url: "https://api.particle.io/v1/devices/{{project_data[step-1]['device_id']}}/{{project_data[step-1]['function']}}?access_token={{access_key}}",
-//           data: "off",
-//           success: function(data, status, xhr){
-//             alert('ok');
-//           },
-//           error: function(xhr, status, err) {
-//             alert(status + ": " + err);
-//           }
-//     });
-//     $.ajax({
-//       type: 'POST',
-//       async: false,
-//       url: "https://api.particle.io/v1/devices/{{project_data[step]['device_id']}}/{{project_data[step]['function']}}?access_token={{access_key}}",
-//       data: "on",
-//       success: function(data, status, xhr){
-//         alert('ok');
-//       },
-//       error: function(xhr, status, err) {
-//         alert(status + ": " + err);
-//       }
-//     });
-//
-//   });
-// });
+function postForms(step){
+  var step = step.toString();
+  var stepPrev = (step-1).toString();
+  var id1="device_id"+step;
+  var id2 = "function"+step;
+  var id3= "device_id"+stepPrev;
+  var id4 = "function"+stepPrev;
+  var device_id = document.getElementById(id1).innerText;
+  var funct = document.getElementById(id2).innerText;
+  var device_idPrev = document.getElementById(id3).innerText;
+  var functPrev = document.getElementById(id4).innerText;
+  $.ajax({
+    type: 'POST',
+    url: "https://api.particle.io/v1/devices/"+device_idPrev+"/"+functPrev,
+    data: {
+      access_token:"{{access_key}}",
+      arg:"off"},
+    success: function(data, status, xhr){
+      console.log("ok")
+    },
+    error: function(xhr, status, err) {
+      console.log("error")
+    }
+  });
+  $.ajax({
+    type: 'POST',
+    url: "https://api.particle.io/v1/devices/"+device_id+"/"+funct,
+    data: {
+      access_token:"{{access_key}}",
+      arg:"on"},
+    success: function(data, status, xhr){
+      console.log("ok")
+    },
+    error: function(xhr, status, err) {
+      console.log("error")
+    }
+  });
+
+};
