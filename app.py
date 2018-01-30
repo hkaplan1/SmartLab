0f060ed7b859f0d271831590fe9588eb5cff931e
Harrison Kaplan <harrisonkaplan@dhcp-18-189-17-195.dyn.MIT.EDU>
2018-01-30 11:22:55 -0500

    implement two option home page

--- app.py
+++ app.py
@@ -6,40 +6,43 @@ access_key = "077ab2a1817aecb1dbacb4cc58439f447eeffc76"
 device1 = "210042001747343338333633"
 device2 = "340037001147353136383631"
 # nested_dictionaries = {projectnumber:{step#:{device_id:x,command:y}}}
-projectData = {1:
-                [{"name":"Project 1"},
-                 {"device_id":device1,"function":"step1"},
-                 {"device_id":device1,"function":"step2"},
-
-                 {"device_id":device2,"function":"step3"},
-                 {"device_id":device2,"function":"step4"}],
-               2:
-                [{"name":"Project 2"},
-                {"device_id":device1,"function":"step1"},
-                {"device_id":device2,"function":"step3"},
-                 {"device_id":device1,"function":"step2"},
-
-                 {"device_id":device2,"function":"step4"}],
-                3:
-                [{"name":"Project 3"},
-                {"device_id":device1,"function":"step2"}]
+projectData = {1:{"name":"Project 1",
+                  "steps":[{"device_id":device1,"function":"step1"},
+                           {"device_id":device1,"function":"step2"},
+                           {"device_id":device2,"function":"step3"},
+                           {"device_id":device2,"function":"step4"}]},
+               2:{"name":"Project 2",
+                  "steps":[{"device_id":device1,"function":"step1"},
+                           {"device_id":device2,"function":"step3"},
+                           {"device_id":device1,"function":"step2"},
+                           {"device_id":device2,"function":"step4"}]},
+                3:{"name":"Project 3",
+                   "steps":[{"device_id":device2,"function":"step4"}]}
                }
-
 
 
 @app.route('/')
 def home():
+    return render_template('hub.html')
+
+@app.route('/themes')
+def theme():
+    return render_template('projects.html')
+
+
+@app.route('/guides')
+def guide():
     projects = []
     for key, value in sorted(projectData.items()):
         print(key,value)
-        projects.append((key,value[0]['name']))
+        projects.append((key,value['name']))
 
-    return render_template('index.html',projects = projects)
+    return render_template('guides.html',projects = projects)
 
 
-@app.route('/projects/', defaults={'project': 1})
-@app.route('/projects/<int:project>')
-def show_users(project):
+@app.route('/guides/', defaults={'project': 1})
+@app.route('/guides/<int:project>')
+def show_project(project):
     return render_template('projects.html',
         access_key=access_key,
         project_data = projectData[project]
